683a598aafa42173b3d3960b69bbc55ff9709252
Olusegun Akinnola <Olusegun.akinnola@wemabank.com>
2022-11-23 12:05:28 +0100

    Add project files.

---
+++ StudentRecords/Controllers/StudentController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StudentRecords.Data;
+using StudentRecords.DTO;
+using StudentRecords.Service;
+
+namespace StudentRecords.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private readonly IStudentDbContext _context;
+        private readonly ImessageProducer _messagePublisher;
+
+        public StudentController(IStudentDbContext context, ImessageProducer messagePublisher)
+        {
+            _context = context;
+            _messagePublisher = messagePublisher;
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateNewStudent(StudentDTO studentDto)
+        {
+            Student student = new()
+            {
+                StudentName = studentDto.StudentName,
+                Age = studentDto.Age,
+                CourseTitle = studentDto.CourseTitle
+            };
+
+            _context.Student.Add(student);
+
+            await _context.SaveChangesAsync();
+
+            _messagePublisher.SendMessage(student);
+
+            return Ok(new { id = student.Id });
+        }
+    }
+}
---
+++ StudentRecords/Service/ImessageProducer.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace StudentRecords.Service
+{
+    public class ImessageProducer
+    {
+        public void SendMessage<T>(T message)
+        {
+            var factory = new ConnectionFactory
+            {
+                HostName = "localhost"
+            };
+            var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            channel.QueueDeclare("students", exclusive: false);
+
+            var json = JsonConvert.SerializeObject(message);
+            var body = Encoding.UTF8.GetBytes(json);
+
+            channel.BasicPublish(exchange: "", routingKey: "students", body: body);
+        }
+    }
+}
